6892950091349ee0c71d17b19c3e9e30af06eb8d
pkbotchway <papakojobotchway1000@gmail.com>
2021-08-05 15:25:40 +0000

    initial commit

---
+++ app/src/main/java/com/example/musicplay/SongFragment.java
@@ -0,0 +1,30 @@
+package com.example.musicplay;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class SongFragment extends Fragment {
+
+
+    public SongFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_song, container, false);
+        return view;
+    }
+}
