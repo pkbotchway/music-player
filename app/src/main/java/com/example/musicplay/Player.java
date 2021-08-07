47b86681c420550a0b2e7f401572922e01ef9cf2
pkbotchway <papakojobotchway1000@gmail.com>
2021-08-07 00:29:58 +0000

    initial commit

---
+++ app/src/main/java/com/example/musicplay/Player.java
@@ -0,0 +1,14 @@
+package com.example.musicplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
