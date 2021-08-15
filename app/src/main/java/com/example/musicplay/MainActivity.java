f46a984a721f6d2ee81772d79b5370257f54da86
pkbotchway <papakojobotchway1000@gmail.com>
2021-08-15 23:51:58 +0000

    update

--- app/src/main/java/com/example/musicplay/MainActivity.java
+++ app/src/main/java/com/example/musicplay/MainActivity.java
@@ -130,7 +130,8 @@ public class MainActivity extends AppCompatActivity {
                 MediaStore.Audio.Media.TITLE,
                 MediaStore.Audio.Media.DURATION,
                 MediaStore.Audio.Media.DATA, //for file path
-                MediaStore.Audio.Media.ARTIST
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media._ID
         };
         Cursor cursor = context.getContentResolver().query(uri, projection,
                 null, null, null);
@@ -143,8 +144,9 @@ public class MainActivity extends AppCompatActivity {
                 String duration = cursor.getString(2);
                 String path = cursor.getString(3);
                 String artist = cursor.getString(4);
+                String id = cursor.getString(5);
 
-                MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration);
+                MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration, id);
                 //take log.e for check
                 Log.e("Path : " + path, "Album : " + album);
                 tempAudioList.add(musicFiles);
